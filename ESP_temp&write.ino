6407bd15b579505dd37217006f3f9ceda00ae6ec
ViniciusCosta1300 <vinicius.costa@icomp.ufam.edu.br>
2026-09-25 15:57:01 -0400

    deleting old files

--- ESP_temp&write.ino
+++ ESP_temp&write.ino
@@ -4,7 +4,7 @@
 #include <SPI.h>
 #include <SD.h>
 
-// Data wire is plugged into port 2 on the Arduino
+// Data wire is plugged into port 2 on the ESP
 #define ONE_WIRE_BUS 7
 
 
@@ -20,6 +20,26 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 SPIClass sdSPI(HSPI);
 
+void rawSdProbe() {
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+  sdSPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+  sdSPI.beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+
+  for (int i = 0; i < 10; i++) sdSPI.transfer(0xFF);  // 80 clocks, CS high
+
+  digitalWrite(SD_CS, LOW);
+  uint8_t cmd0[] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
+  for (uint8_t b : cmd0) sdSPI.transfer(b);
+
+  uint8_t r = 0xFF;
+  for (int i = 0; i < 10 && r == 0xFF; i++) r = sdSPI.transfer(0xFF);
+
+  digitalWrite(SD_CS, HIGH);
+  sdSPI.endTransaction();
+  Serial.printf("CMD0 response: 0x%02X\n", r);
+}
+
 /*
  * The setup function. We only start the sensors here
  */
@@ -28,13 +48,18 @@ void setup(void)
   // start serial port
   Serial.begin(115200);
   Serial.println("Dallas Temperature IC Control Library Demo");
-
+/*
+  for (int i = 0; i < 30; i++) {
+  rawSdProbe();
+  delay(300);
+}
+  */
   // Start up the library
   sensors.begin();
   sdSPI.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
-  if (!SD.begin(SD_CS, sdSPI)) {
+  while (!SD.begin(SD_CS, sdSPI)) {
     Serial.println("SD Card Mount Failed!");
-    return;
+    delay(200);
   }
   uint8_t cardType = SD.cardType();
   if (cardType == CARD_NONE) {
@@ -75,11 +100,15 @@ void loop(void) {
       Serial.print(tempC);
       Serial.println(" °C");
     } else {
-      Serial.println("Error: Could not open /data.csv for writing.");
+      while(!csvFile) {
+        csvFile = SD.open("/data.csv", FILE_APPEND);
+        Serial.println("Error: Could not open /data.csv for writing.");
+        delay(200);
+      }
     }
   } else {
     Serial.println("Error: DS18B20 disconnected. Check 4.7k resistor & wiring.");
   }
 
-  delay(2000); // Adjust sample interval as needed
+  delay(900000); // Adjust sample interval as needed
 }
